6adc692a58c41c83699ab3a0c0d42df58829f7ce
Syed Ziaul Habib <roobon@gmail.com>
2022-12-27 02:19:36 +0600

    Backend driven by CodeIgniter and design with AdminLTE template

---
+++ backend/app/Database/Migrations/2022-12-26-110445_Products.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Products extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            "id" => [
+                'type' => "INT",
+                'constraint' => 12,
+                'auto_increment' => true
+            ],
+            "product_name" => [
+                'type' => "VARCHAR",
+                'constraint' => 100,
+                'null' => false,
+            ],
+            "product_details" => [
+                'type' => "VARCHAR",
+                'constraint' => 300,
+                'null' => true,
+            ],
+            "product_price" => [
+                'type' => "DECIMAL",
+                'constraint' => '10, 2',
+
+            ]
+        ]);
+        $this->forge->addKey("id", true);
+        $this->forge->createTable("products");
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('products');
+    }
+}
---
+++ backend/app/Views/products/product_list.php
@@ -0,0 +1,88 @@
+    <?php echo view('layouts/header1.php') ?>
+
+    <!-- Navbar -->
+    <?php echo view('layouts/top_nav.php') ?>
+    <!-- /.navbar -->
+
+    <!-- Main Sidebar Container -->
+    <?php echo view('layouts/main_sidebar.php') ?>
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Dashboard</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Dashboard v1</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <!-- Small boxes (Stat box) -->
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">DataTable with default features</h3>
+                            </div>
+                            <!-- /.card-header -->
+                            <div class="card-body">
+                                <table id="example1" class="table table-bordered table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Product Name</th>
+                                            <th>Details</th>
+                                            <th>Price</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php foreach ($products as $product) : ?>
+                                            <tr>
+                                                <td><?= $product['id']; ?></td>
+                                                <td><?= $product['product_name']; ?>
+                                                </td>
+                                                <td><?= $product['product_details']; ?></td>
+                                                <td><?= $product['product_price']; ?></td>
+                                                <td>
+                                                    <a href="" class="mx-2"><i class="fa fa-pen"></i></a>
+                                                    <a href=""><i class="fa fa-trash"></i></a>
+                                                </td>
+                                            </tr>
+                                        <?php endforeach; ?>
+                                    </tbody>
+                                    <tfoot>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Product Name</th>
+                                            <th>Details</th>
+                                            <th>Price</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </tfoot>
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+    <?php echo view('layouts/footer1.php') ?>
